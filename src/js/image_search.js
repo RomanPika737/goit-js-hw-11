771ab8b73120db485a8f697863bb0fafa9f98d67
Roman <pika1roman@gmail.com>
2023-03-28 18:45:14 +0200

    first step

--- src/js/image_search.js
+++ src/js/image_search.js
@@ -1,2 +1,43 @@
-// import Notiflix from 'notiflix';
-// const axios = require('axios').default;
+import Notiflix from 'notiflix';
+import axios from "axios";
+
+
+const API_KEY = '34818216-83cfe1c4dc113fc342b3737b5';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const form = document.querySelector('#search-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchQuery = document.querySelector('input').value.trim();
+  const url = `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // handle the response data here
+        // renderCollection(data.hits);
+        // return data.hits;
+        createPhoto(data.hits)
+    })
+    .catch((error) => console.error(error));
+});
+
+
+function createPhoto(collection) {
+    const gallery = document.querySelector('.gallery');
+gallery.innerHTML = ''; // clear the previous search results
+collection.forEach((image) => {
+  const card = document.createElement('div');
+  card.classList.add('photo-card');
+  card.innerHTML = `
+    <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+      <p class="info-item"><b>Views:</b> ${image.views}</p>
+      <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+      <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+    </div>
+  `;
+  gallery.appendChild(card);
+});
+
+}
